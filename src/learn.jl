b67059817a24b5302558b8df7fae2272f96ed4a2
Nicholas Moore <nsmoore57@gmail.com>
2020-07-19 17:18:39 -0500

    Move training logic to its own function

--- src/learn.jl
+++ src/learn.jl
@@ -1,8 +1,7 @@
-# TODO: use a struct or named tuple to handle all these arguments
-
 function learn!(env::E, qpolicy::Q, num_eps, γ;
-                maxn=200, update_freq=3000, chkpt_freq=3000, replay_buffer_size=10000,
-                train_batch_size=1000, render=false, plot_freq=3000) where {E<:Reinforce.AbstractEnvironment, Q<:QPolicy}
+                maxn=200, opt=ADAM(0.0001), update_freq=3000, chkpt_freq=3000,
+                replay_buffer_size=10000, train_batch_size=1000, render=false,
+                plot_freq=3000) where {E<:Reinforce.AbstractEnvironment, Q<:QPolicy}
     # Build an epsilon greedy policy for the learning
     π = ϵGreedyPolicy(1.0, qpolicy)
 
@@ -11,12 +10,6 @@ function learn!(env::E, qpolicy::Q, num_eps, γ;
 
     # Replay Buffer
     mem = PriorityReplayMemoryBuffer(replay_buffer_size)
-
-    # ADAM Optimizer
-    opt = ADAM(0.0001)
-
-    # Standard Gradient Descent Optimizer
-    # opt = Descent(0.001)
 
     # Params to optimize
     p = get_params(qpolicy)
@@ -35,37 +28,8 @@ function learn!(env::E, qpolicy::Q, num_eps, γ;
             # Fill the buffer before training or lowering ϵ
             length(mem) < train_batch_size && continue
 
-            # Training..........
-
-            # decrease ϵ to be 10% exploration and 90% exploitation
-            # TODO: Need a better way/place to control this
-            #       Could make a function called decrease_ϵ(π) which is part of the ϵ greedy policy
-            π.ϵ = 0.1
-
-            # Sample a batch from the replay buffer
-            (s_batch, a_batch, r_batch, s′_batch, done_batch, ids, weights) = sample(mem, train_batch_size)
-
-            # Get the target values based on the next states
-            target = get_target(qpolicy, γ, r_batch, done_batch, s′_batch)
-
-            # May need this to help set the data type for loss - not sure
-            loss = 0.0
-
-            # Track the td errs
-            td_errs = similar(target)
-
-            # Track gradients while calculating the loss
-            gs = Flux.gradient(p) do
-                currentQ_SA = get_QValues(qpolicy, s_batch)[a_batch]
-                td_errs = currentQ_SA .- target
-                loss = Flux.huber_loss(td_errs.*weights)
-            end
-
-            # Train the network(s)
-            Flux.Optimise.update!(opt, p, gs)
-
-            # Update Priorities for selected memory elements
-            update_priorities!(mem, ids, td_errs)
+            # Training
+            td_errs = trainWithBatch!(π, mem, train_batch_size, qpolicy, γ, p, opt)
 
             # If needed, update the target Network
             update_freq > 0 && step % update_freq == 0 && update_target(qpolicy)
@@ -82,8 +46,6 @@ function learn!(env::E, qpolicy::Q, num_eps, γ;
 
             step += 1
         end # end of episode
-        # PlotPolicy(qpolicy, 1000, 0.001)
-        # println("Total Reward After Episode $i: $(ep.total_reward)")
 
         # Anneal β linearly toward 1.0
         mem.β -= (1.0 - mem.β0)/num_eps
@@ -92,3 +54,35 @@ function learn!(env::E, qpolicy::Q, num_eps, γ;
     num_successes, losses
 end
 
+function trainWithBatch!(π, mem, train_batch_size, qpolicy, γ, params, opt)
+    # Decrease ϵ
+    π.ϵ = 0.1
+
+    # Sample a batch from the replay buffer
+    (s_batch, a_batch, r_batch, s′_batch, done_batch, ids, weights) = sample(mem, train_batch_size)
+
+    # Get the target values based on the next states
+    target = get_target(qpolicy, γ, r_batch, done_batch, s′_batch)
+
+    # May need this to help set the data type for loss - not sure
+    loss = 0.0
+
+    # Track the td errs
+    td_errs = similar(target)
+
+    # Track gradients while calculating the loss
+    gs = Flux.gradient(params) do
+        currentQ_SA = get_QValues(qpolicy, s_batch)[a_batch]
+        td_errs = currentQ_SA .- target
+        loss = Flux.huber_loss(td_errs.*weights)
+    end
+
+    # Train the network(s)
+    Flux.Optimise.update!(opt, params, gs)
+
+    # Update Priorities for selected memory elements
+    update_priorities!(mem, ids, td_errs)
+
+    # return the td_errs so we can save them for later
+    return td_errs
+end
